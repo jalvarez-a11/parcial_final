4ce2cddb6d05a4351027305e03395e3f68be28b7
jose manuel alvarez <josealeman93@hotmail.com>
2024-11-29 20:21:23 -0500

    llenada de matriz con nuemros hasta el nueve

---
+++ parcial_final/parcial_final/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el número de filas (N):");
+        int filas = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el número de columnas (M):");
+        int columnas = int.Parse(Console.ReadLine());
+
+        // Crear la matriz
+        int[,] matriz = new int[filas, columnas];
+
+        // Llenar la matriz con números aleatorios
+        Random random = new Random();
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                matriz[i, j] = random.Next(1, 10); // Números aleatorios entre 1 y 9
+            }
+        }
+
+        // Imprimir la matriz
+        Console.WriteLine("La matriz generada es:");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+ }
